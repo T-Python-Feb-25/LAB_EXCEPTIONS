802fac0554f990947a649fe235575b02cf1eac67
elaf <elafabdualrahman00@gmail.com>
2025-02-09 13:12:32 +0300

    lab is solved

--- lab.py
+++ lab.py
@@ -1,27 +1,11 @@
-'''
-Below you have a code that raises an exception , using what you learned do the following:
-- Find what type of exception is raised.
-- Hanlde the exception in try..except 
-- If operation successful , print "the operation is successful"
-- if operation fails, handle the specific exception that is raised , and print a relevant message.
-```
 def additoin(x, y):
     x = 10
     y = 20
     print("Addition:", x + b)
 
-
-additoin(10, 20)
-'''
-def additoin(x, y):
-    x = 10
-    y = 20
-    print("Addition:", x + y)
-
 #Task 1: Find what type of exception is raised
 try:
     additoin(10, 20)
-    print("The operation is successful")
+    print("The operation is successful") #Task 3: If operation successful , print "the operation is successful"
 except NameError as ne: #Task 2: Handle the exception in try..except
-    print("A NameError occurred: ", ne)
-    
+    print(f"A NameError occurred: {ne}. Try again") #Task 4: if operation fails, handle the specific exception that is raised , and print a relevant message.
