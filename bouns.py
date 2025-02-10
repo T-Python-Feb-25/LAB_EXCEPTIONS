56f25abc03afa7f848c94eb70c245be090d0e769
raghad algarni <raghad.algarni601@gmail.com>
2025-02-10 04:31:15 +0300

    lab_exceptions

--- bouns.py
+++ bouns.py
@@ -1,28 +1,39 @@
 def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
+    return (celsius * 9 / 5) + 32
 
 def fahrenheit_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * 5/9
+    return (fahrenheit - 32) * 5 / 9
 
 def main():
     while True:
         user_input = input('Enter a temperature and its unit (e.g., "25 C" or "77 F"): ')
         try:
             value, unit = user_input.split()
-            value = float(value)  
+
+            if not value.isdigit():
+                raise ValueError("Invalid temperature value. Please enter a numeric value.")
+            
+            value = float(value) 
+
+            if unit.upper() not in ['C', 'F']:
+                raise TypeError("Invalid unit. Please use 'C' for Celsius or 'F' for Fahrenheit.")
 
             if unit.upper() == 'C':
                 converted = celsius_to_fahrenheit(value)
-                print(f"Temperature in Fahrenheit: { converted:.2f} F")
+                print(f"Temperature in Fahrenheit: {converted:.2f} F")
             elif unit.upper() == 'F':
                 converted = fahrenheit_to_celsius(value)
                 print(f"Temperature in Celsius: {converted:.2f} C")
-            else:
-                raise TypeError("Invalid unit. Please use 'C' for Celsius or 'F' for Fahrenheit.")
 
-        except ValueError:
-            print("Invalid temperature value. Please enter a numeric value.")
+        except ValueError as e:
+            print(e)
         except TypeError as e:
             print(e)
+        except Exception as e:
+            print("An unexpected error occurred:", e)
+        else:
+            print("the operation is successful")
+
 if __name__ == "__main__":
     main()
+             
--- lab_exceptions.py
+++ lab_exceptions.py
@@ -1,12 +1,4 @@
-#Find what type of exception is raised.
-#Hanlde the exception in try..except
-#If operation successful , print "the operation is successful"
-#if operation fails, handle the specific exception that is raised , and print a relevant message.
-#def additoin(x, y):
-    #x = 10
-    #y = 20
-    #print("Addition:", x + b)
-#additoin(10, 20)
+
 try:
     def additoin(x, y):
         x = 10
@@ -14,7 +6,7 @@ try:
         print("Addition:", x + b )
     additoin(10, 20)
 except NameError as w:
-    print("pleas enter only two numbers, b is not define")
+    print("pleas enter only two varible, b is not define")
 except Exception as w:
     print(w.__class__)
 #another examele for error
@@ -29,15 +21,3 @@ else:
 
 
 
-
-
-#try:
-    #num1:int =int( input("pleas entre any num to multiply it : "))
-    #num2:int=int(input("lpeas entre sec num to multiply it : "))
-    #reslt:int = num1*num2
-   # print(reslt)
-#except ValueError:
- #   print("provid num")
-#except Exception as e:
-   # print(e.__class__)
-#print("yeeaaaaiiiiiiii")
